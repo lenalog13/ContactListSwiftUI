ef741a195e614cb0a2ec40749e28b424355adc4a
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-06-16 16:56:55 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Елена Логинова on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
