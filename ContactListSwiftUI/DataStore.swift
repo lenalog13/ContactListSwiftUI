9ee0daa4fb8c1e5158c5b91fd7b7f24eb9245f3a
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-06-18 10:59:03 +0300

    create DataStore

---
+++ ContactListSwiftUI/DataStore.swift
@@ -0,0 +1,33 @@
+//
+//  DataStore.swift
+//  ContactListSwiftUI
+//
+//  Created by Елена Логинова on 18.06.2023.
+//
+
+final class DataStore {
+    
+    static let shared = DataStore()
+    
+    let names = ["Lena", "Kate", "Masha",
+                 "Pasha", "Stas", "Kolyn",
+                 "Alex", "Zhenya", "Mishel",
+                 "Jane", "Fiona", "Karl"]
+    
+    let surnames = ["Choi", "Ten", "Chaun",
+                    "Black", "White", "Pete",
+                    "Voight", "Jonas", "Kardashian",
+                    "Gallager", "Milkovich", "Ronaldy"]
+    
+    let phoneNumbers = ["111222333", "222333444", "333444555",
+                        "444555666", "555666777", "666777888",
+                        "777888999", "888999000", "999000111",
+                        "000111222", "123456789", "987654321"]
+    
+    let emails = ["kivi@mail.ru", "lichi@yandex.com", "kino@gmail.com",
+                  "slice@rambler.ru", "noise@yandex.ru", "titanus@rambler.com",
+                  "salt@mail.ru", "miu-miu@gmail.com", "super@mail.ru",
+                  "kikimora@yandex.com", "vedma@rambler.com", "kiss@gmail.ru"]
+    
+    private init() {}
+}
