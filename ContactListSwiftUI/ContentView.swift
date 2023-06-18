fcd568662fed8d1cb09414ac542bf3ee3e4e2f52
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-06-18 11:35:52 +0300

    setting tabView

---
+++ ContactListSwiftUI/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  TabView.swift
+//  ContactListSwiftUI
+//
+//  Created by Елена Логинова on 16.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private let contactList = Person.getContactList()
+    
+    var body: some View {
+        TabView {
+            ContactsView()
+                .tabItem {
+                    Image(systemName: "person.2.fill")
+                    Text("Contacts")
+                }
+            NumbersView()
+                .tabItem {
+                    Image(systemName: "phone.fill")
+                    Text("Numbers")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
